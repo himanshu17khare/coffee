770cfe5512f83505aed920586430b0f9b26811b4
Khare4Developer <khare4developer01@gmail.com>
2018-11-21 01:37:46 +0530

    first commit

---
+++ app/src/main/java/com/example/aditya/bookyourcoffee/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.aditya.bookyourcoffee;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                   startActivity(new Intent(MainActivity.this,CoffeeActivity.class));
+                   finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
